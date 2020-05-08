d85be22b2ecad662ecaa4fd9248a70fc1331d67b
raymond vu <v.anhvn13@cloudhms.net>
2020-05-09 00:55:00 +0700

    Updates

--- precache-manifest.a789d997a2d0f07a40b5a30d5d0010a5.js
+++ precache-manifest.a789d997a2d0f07a40b5a30d5d0010a5.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "af01e29e0f1e8bea2ef463ea31101076",
+    "revision": "9fb91da2adbc0289845bae102f5724e3",
     "url": "/myapp/index.html"
   },
   {
-    "revision": "6106115e7642ee78e7a1",
+    "revision": "c5cf0978bbbf70a35587",
     "url": "/myapp/static/css/main.874f089d.chunk.css"
   },
   {
-    "revision": "aafc7c30f557aff89bf2",
-    "url": "/myapp/static/js/2.e8f331a3.chunk.js"
+    "revision": "65d7c5cafa9196dccd17",
+    "url": "/myapp/static/js/2.fd569b7b.chunk.js"
   },
   {
     "revision": "e4279675a9c60179159c82343fcf3639",
-    "url": "/myapp/static/js/2.e8f331a3.chunk.js.LICENSE.txt"
+    "url": "/myapp/static/js/2.fd569b7b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6106115e7642ee78e7a1",
-    "url": "/myapp/static/js/main.cc06123d.chunk.js"
+    "revision": "c5cf0978bbbf70a35587",
+    "url": "/myapp/static/js/main.adac72a1.chunk.js"
   },
   {
     "revision": "801571b283feffcd85c2",
